1417d097cabc2511398d4cfdf4482eb242f90a83
殇單羽 <744179636@qq.com>
2026-05-25 02:44:23 +0800

    Canvas层级管理器初始化流程上传
    Canvas层级管理器初始化流程上传

--- Assets/2_Scripts/Framework/Core/Manager/ManagerOfManager.cs
+++ Assets/2_Scripts/Framework/Core/Manager/ManagerOfManager.cs
@@ -8,12 +8,14 @@ public class ManagerOfManager : Singleton<ManagerOfManager>
     public override void InitDataM()
     {
         CfgToolManager.Instance.Init();
+        CanvasManager.Instance.InitDataM();
         //GameDataManager.Instance.InitDataM();
     }
 
     public override void DestroyM()
     {
         CfgToolManager.Instance.Destroy();
+        CanvasManager.Instance.DestroyM();
         //GameDataManager.Instance.DestroyM();
     }
 }
